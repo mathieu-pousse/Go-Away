912cd21fb584f109a27509d04019387a56eef009
Watad Wallah <nyah.check@ubuea.cm>
2016-07-14 00:20:06 +0100

    Added hello world program.

---
+++ src/hello.go
@@ -0,0 +1,17 @@
+/**
+ * Filename: Hello.go
+ * Author: Nyah Check
+ * Usage: Hello world in Golang
+ * Licence: GNU PL 2016
+ * Buea, Cameroon.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("hello, world")
+
+}
