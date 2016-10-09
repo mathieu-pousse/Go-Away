7a9ecdf09e6e7173e79599304bb415b8040da766
Nyah Check <check.nyah@gmail.com>
2016-10-09 22:28:37 +0100

    Added experiments on channels.

---
+++ hello/channels.go
@@ -0,0 +1,42 @@
+/**
+ * Filename: channels.go
+ * Author: Nyah Check
+ * Usage: Uses channels to synchronize data between processes.
+ * Licence: GNU PL 2016
+ */
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+
+type Worker struct {
+	id int
+}
+
+//Worker function for channeling data.
+func (w Worker) process(c chan int) {
+
+	data := <- c
+	fmt.Printf("\nWorker %d, received %d data.", w.id, data)
+}
+
+func main() {
+
+	c := make (chan int)
+	for i:= 0; i < 5; i++ {
+		worker := &Worker{id: i}
+		go worker.process(c)//passes data to channel for processing
+	
+	}
+	
+	for {
+		c <- rand.Int()
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+
